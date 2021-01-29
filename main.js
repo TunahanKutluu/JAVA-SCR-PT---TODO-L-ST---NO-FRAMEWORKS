b2a41fa297790e093e7b01d2f82911a9b4b251f8
Tunahan Kutlu <tunahannkutlu@gmail.com>
2021-01-29 13:20:13 +0300

    TODO LİST ( JS ) NO FRAMEWORKS

---
+++ main.js
@@ -0,0 +1,28 @@
+function  todoList(){
+    
+    const list =document.querySelector('.list');
+    const input =document.querySelector('input');
+    const newTask=document.createElement('li');
+    const span =document.createElement('span');
+
+    span.innerHTML = '<i class="fas fa-trash"></i>';
+
+    if(input.value !==''){
+        newTask.textContent = input.value;
+        input.value = '';
+        list.appendChild(newTask);
+        newTask.appendChild(span);
+    }
+
+    span.addEventListener('click' , function(){
+
+        const parent = this.parentNode;
+        parent.remove();
+    });
+
+    newTask.addEventListener('click', () =>{
+
+        newTask.classList.toggle('complated');
+    });
+
+}
